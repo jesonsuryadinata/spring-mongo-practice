fcfdc17d905f5ccdf79149509ed9c1e0688b6b65
Jeson <jeson.suryadinata@yahoo.com>
2023-04-13 08:54:32 +0700

    First commit

---
+++ src/main/java/com/example/springbootmongodbtutorial/SpringBootMongodbTutorialApplication.java
@@ -0,0 +1,14 @@
+package com.example.springbootmongodbtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+SpringBootMongodbTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMongodbTutorialApplication.class, args);
+	}
+
+}
